d40f4cdba248b15e45af8bb2613202c53e49e3f5
ImtheH <hoangnvph49147@gmail.com>
2024-12-02 18:41:20 +0700

    hoang

--- Du_An_1/admin/controller/billcontroller.php
+++ Du_An_1/admin/controller/billcontroller.php
@@ -12,10 +12,11 @@ class billController
         require_once "../commons/function.php";
         require_once "view/listBill.php";
     }
-    function updateBill($id){
+    function updateBill($id)
+    {
         $bills = $this->billModel->bill();
         $oneBill = $this->billModel->findBillById($id);
-        $status = $this->billModel->billStatus($id);
+        $status = $this->billModel->billStatus($id)['status'];
         $statusDescriptions = [
             0 => "Chờ xác nhận",
             1 => "Đã xác nhận",
@@ -28,13 +29,24 @@ class billController
         require_once "../commons/function.php";
         require_once "view/updateBill.php";
         if (isset($_POST['btn_update'])) {
-            $status = $_POST['status'];
-            if ($this->billModel->updateBill($status,$id)) {
-                header("Location:?act=listBill");
+            $newStatus = $_POST['status']; // Lấy trạng thái mới từ form
+            if ($newStatus < $status) {
+                echo "<script>alert('Không thể cập nhật trạng thái lùi về trước!');</script>";
             } else {
-                echo "Sửa thất bại";
+                if ($newStatus == 6 && $status == 5) {
+                    echo "<script>alert('Đơn hàng đã giao thành công, không thể hủy!');</script>";
+                } else {
+                    if ($newStatus == 5 && $status != 5) { // Trạng thái chuyển thành 'Giao hàng thành công'
+                        $this->billModel->reduceQuantity($id);
+                    }
+                    if ($this->billModel->updateBill($newStatus, $id)) {
+                        header("Location:?act=listBill");
+                    } else {
+                        echo "Sửa thất bại";
+                    }
+                }
             }
         }
     }
-    
+
 }
