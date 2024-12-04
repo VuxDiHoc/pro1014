4f4955745be51438c8e6690fc26c12f58a7b145f
datnguyen20051908 <datntph49177@gmail.com>
2024-12-04 19:49:49 +0700

    sualaivalidatedangnhap

--- Du_An_1/controller/logincontroller.php
+++ Du_An_1/controller/logincontroller.php
@@ -9,16 +9,19 @@ class LoginController
             $email = $_POST['email'];
             $password = $_POST['password'];
 
+            // Giả sử bạn có hàm để kiểm tra thông tin đăng nhập
             $user = checkLogin($email, $password);
 
             if ($user) {
                 $_SESSION['user'] = $user;
+                // Chuyển hướng đến trang chủ hoặc trang mong muốn sau khi đăng nhập thành công
                 header('Location: index.php');
                 exit;
             } else {
-                $error = "Email hoặc mật khẩu không đúng";
+                $error = "Thông tin đăng nhập không đúng.";
             }
         }
         require_once 'view/login.php';
     }
 }
+
